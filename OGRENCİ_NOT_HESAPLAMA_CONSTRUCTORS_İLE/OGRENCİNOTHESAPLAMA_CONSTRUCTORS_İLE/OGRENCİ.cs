8a9d04ad1ee258d51d5409e3a0b3c1ae219d9dea
Serkan <serkankabadayiogullari8233@gmail.com>
2023-08-23 18:18:52 +0300

    OGRENCİ_NOT_HESAPLAMA_OOP
    ÖĞRENCİ NOT HESAPLAMASINI VE ORTLAMASINA GÖRE GEÇME KALMA DURMUNU SORGULAYAN KODU YAZDIM

---
+++ OGRENCİ_NOT_HESAPLAMA_CONSTRUCTORS_İLE/OGRENCİNOTHESAPLAMA_CONSTRUCTORS_İLE/OGRENCİ.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGRENCİNOTHESAPLAMA_CONSTRUCTORS_İLE
+{
+    public class OGRENCİ
+    {
+        //SABİT ALANLAR
+        const decimal _VIZECARPAN1 = 0.4M;
+        const decimal _VIZECARPAN2 = 0.4M;
+        const decimal _FİNAL = 0.6M;
+
+        //HAREKETLİ ALANLAR
+        decimal _vize1;
+        decimal _vize2;
+        decimal _final;
+        decimal _ortalama;
+        string _durum;
+
+
+        //ÖZELLİK
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+
+        public OGRENCİ(string adi, string soyadi, decimal vize1, decimal vize2, decimal final)
+        {
+            Adi = adi;
+            Soyadi = soyadi;
+            _vize1 = vize1;
+            _vize2= vize2;
+            _final = final;
+        }
+        public void OrtalamaHesapla()
+        {
+            _ortalama = (_VIZECARPAN1 * _vize1) + (_VIZECARPAN2 * _vize2) + (_FİNAL * _final);
+            _durum = "Kaldı";
+        }
+        public string Getir()
+        {
+            string sonuc = $"AD ve SOYAD:{Adi}{Soyadi}";
+            sonuc += $"\nNot Ortalaması:{_ortalama.ToString("N1")}";
+            sonuc += $"\nDurumu:\"{_durum}\"";
+            return sonuc;
+
+        }
+    }
+}
+
+
+
+
---
+++ OGRENCİ_NOT_HESAPLAMA_CONSTRUCTORS_İLE/OGRENCİNOTHESAPLAMA_CONSTRUCTORS_İLE/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OGRENCİNOTHESAPLAMA_CONSTRUCTORS_İLE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BİR ÖĞRENCİNİN VİZE1, VİZE2, FİNAL SINAVLARININ ORTALAMASINA GÖRE DURUMUNU SÖYLEYEN KOD
+            OGRENCİ ogrenci = new OGRENCİ("Serkan", "Kabadayıoğulları",50,40,30 );
+            ogrenci.OrtalamaHesapla();
+            Console.WriteLine(ogrenci.Getir());
+
+        }
+    }
+}
